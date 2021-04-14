964a3eaf475f603044150c0345320f6e22313413
mikepowertech79 <mikepowertech79@gmail.com>
2021-04-14 21:50:35 +0800

    ok

--- Program.cs
+++ Program.cs
@@ -12,6 +12,44 @@ namespace ConsoleAppFEEZBUZZ
         static void Main(string[] args)
         {
 
+
+
+
+            int[] a = {2, 5, 112, 2342, 8, 1, 4, 6};
+
+            foreach (var i in a)
+            {
+                string stringVar = i.ToString();
+                 
+                Console.Write(stringVar + " ");
+            }
+
+            int t;
+
+            for (int i = 0; i <= a.Length -2 ; i++)
+            {
+                for (int j = 0; j <= a.Length - 2; j++)
+                { 
+                    if (a[j] > a[j+1])
+                    {
+                        t = a[j + 1];
+                        a[j + 1] = a[j];
+                        a[j] = t;
+                    }
+                }
+            }
+
+            Console.Write("\n");
+
+            foreach (var i in a)
+            {
+                string stringVar = i.ToString();
+
+                Console.Write(stringVar + " ");
+            }
+
+
+
             for (int i = 0; i <= 100; i++)
             {
                 var output = "";
